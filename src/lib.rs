eb60857dbc451ea223c51ef307bd28c3cf1882e9
Andrey Yudenko <a.yudenko@21vek.by>
2022-05-04 17:53:13 +0300

    Add .env file. Use data from .env file

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,58 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use dotenv::dotenv;
+
+struct Config {
+    ssl_key_path: String,
+    ssl_cert_path: String,
+    workers: usize,
+    host: String,
+    port: u16,
+}
+
+impl Config {
+    fn new() -> Result<Self, String> {
+        dotenv().ok();
+
+        let ssl_key_path = dotenv::var("SSL_KEY_PATH").unwrap();
+        let ssl_cert_path = dotenv::var("SSL_CERT_PATH").unwrap();
+        let workers = dotenv::var("WORKERS").unwrap().parse::<usize>().unwrap();
+        let host = dotenv::var("HOST").unwrap();
+        let port = dotenv::var("PORT").unwrap().parse::<u16>().unwrap();
+
+        Ok(Config {
+            ssl_key_path,
+            ssl_cert_path,
+            workers,
+            host,
+            port,
+        })
+    }
+}
 
 pub struct APIGateway {
-    port: u16,
-    workers: usize,
+    config: Config,
 }
 
 impl APIGateway {
-    pub fn new(port: u16, workers: usize) -> Self {
-        APIGateway { port, workers }
+    pub fn new() -> Self {
+        let config = Config::new();
+        APIGateway { config: config.unwrap() }
     }
 
     pub async fn run(&self) -> std::io::Result<()> {
-        println!("Starting http server: 127.0.0.1:{} with {} workers.", self.port, self.workers);
+        println!("{}", format!(
+            "Starging http server: {}:{} with {} workers.",
+            self.config.host,
+            self.config.port,
+            self.config.workers,
+        ));
+
+        let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+        builder
+            .set_private_key_file(&self.config.ssl_key_path, SslFiletype::PEM)
+            .unwrap();
+        builder.set_certificate_chain_file(&self.config.ssl_cert_path).unwrap();
 
         HttpServer::new(|| {
             App::new()
@@ -19,8 +60,8 @@ impl APIGateway {
                 .service(echo)
                 .route("/hey", web::get().to(manual_hello))
         })
-        .bind(("127.0.0.1", self.port))?
-        .workers(self.workers)
+        .bind_openssl(format!("{}:{}", self.config.host, self.config.port), builder)?
+        .workers(self.config.workers)
         .run()
         .await
     }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use picasso_api_gateway::APIGateway;
 
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
-    let app = APIGateway::new(5000, 8);
+    let app = APIGateway::new();
     app.run().await
 }
 
